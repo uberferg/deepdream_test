b5cb727972b64d1b41e9daeab82a5cb32237bd58
Andrew Ferg <uberferg@gmail.com>
2016-05-12 05:07:56 -0400

    final commit. ready to submit.

--- scripts/classify_test.py
+++ scripts/classify_test.py
@@ -5,23 +5,23 @@ import caffe
 
 # Set the right path to your model definition file, pretrained model weights,
 # and the image you would like to classify.
-MODEL_FILE = '/home/christopher/Dropbox/2015-16PrincetonUniversity/COS495/Final Project/project_code/models/bvlc_googlenet/deploy.prototxt'
-PRETRAINED = '/home/christopher/Dropbox/2015-16PrincetonUniversity/COS495/Final Project/project_code/models/bvlc_googlenet/bvlc_googlenet.caffemodel'
+MODEL_FILE = '../models/placesCNN/places205CNN_deploy.prototxt'
+PRETRAINED = '../models/placesCNN/places205CNN_iter_300000.caffemodel'
 
 
 caffe.set_mode_cpu()
 net = caffe.Classifier(MODEL_FILE, PRETRAINED,
-                       mean=np.load('//home/christopher/caffe-master/python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1),
                        channel_swap=(2,1,0),
                        raw_scale=255,
                        image_dims=(256, 256))
 #plt.imshow(input_image)
 
 for imageName in ["baseball", "bedroom", "canyon", "city", "car", "chair", "dog", "forest_castle"]:
-    input_image = caffe.io.load_image("/home/christopher/Dropbox/2015-16PrincetonUniversity/COS495/Final Project/project_code/input_files/priors/" + imageName + ".jpg")
+    input_image = caffe.io.load_image("../input_files/priors/" + imageName + ".jpg")
 
     prediction = net.predict([input_image])  # predict takes any number of images, and formats them for the Caffe net automatically
+    print '\n', imageName
     print 'prediction shape:', prediction[0].shape
     plt.plot(prediction[0])
-    print 'predicted class:', prediction[0].argmax()
+    print 'predicted class:', prediction[0].argmax(), max(prediction[0])
     plt.show()
